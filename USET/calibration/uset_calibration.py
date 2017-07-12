d8d7e56c82ecf6483d88aa849a737cb006aa5a72
Raphael Attie <attie.raphael@gmail.com>
2017-07-12 11:10:13 -0400

    idem

--- USET/calibration/uset_calibration.py
+++ USET/calibration/uset_calibration.py
@@ -104,9 +104,9 @@ def calibrate(data_dir, output_dir, fits_extension, dark_path, limb_cleanup, lev
             centered_image2 = np.array(centered_image)
 
             # Apply mask and enhance intensity beyond limb
-            centered_image2[pixels_off_limb] = centered_image2[pixels_off_limb] * 3
+            centered_image2[pixels_off_limb] = centered_image2[pixels_off_limb] * 4
             # Get the maximum intensity for the rescaled image based on 99.99% percentile
-            new_max = compute_intensity_high(centered_image, 99.999)
+            new_max = compute_intensity_high(centered_image, 99.99)
             # Apply rotation of solar rotation axis to the image y-axis (top-bottom axis)
             centered_image2 = align_to_solar_north(centered_image2, header)
             # load a colormap for the preview function (could be passed as argument to this function...)
